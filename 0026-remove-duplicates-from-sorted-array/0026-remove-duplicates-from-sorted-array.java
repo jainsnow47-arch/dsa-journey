593e8198818027d565283af4660b7c829d14e16e
jainsnow47-arch <jainsnow47@gmail.com>
2026-07-05 21:18:09 +0530

    Time: 2 ms (8%), Space: 46.6 MB (62.34%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        int idx = 0;
+        for(int i=0;i<nums.length;i++){
+            if(!set.contains(nums[i])){
+                set.add(nums[i]);
+                nums[idx++] = nums[i];
+            }
+        }
+        return idx;
+    }
+}
