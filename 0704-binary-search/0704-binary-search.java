07d6942913c4cb475087fa2c3b78dfebd2578c0b
jainsnow47-arch <jainsnow47@gmail.com>
2026-08-15 15:29:15 +0530

    Time: 0 ms (100%), Space: 48.1 MB (85.83%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,20 +1,35 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int n = nums.length;
-        int low = 0;
-        int high = n-1;
-        while(low<=high){
-            int mid = low + (high - low)/2;
-            if(nums[mid]==target){
-                return mid;
-            }
-            else if(nums[mid]<target){
-                low = mid + 1;
-            }
-            else{
-                high = mid - 1;
-            }
+    public int bs(int[] arr,int low,int high,int target){
+        if(low>high){ //base case
+            return -1;
         }
-        return -1;
+        int mid = low + (high-low)/2;
+        if(arr[mid]==target){
+            return mid;
+        }
+        else if(arr[mid]<target){
+            return bs(arr,mid+1,high,target);
+        }
+        return bs(arr,low,mid-1,target);
+    }
+    public int search(int[] nums, int target) {
+        // int n = nums.length;
+        // int low = 0;
+        // int high = n-1;
+        // while(low<=high){
+        //     int mid = low + (high - low)/2;
+        //     if(nums[mid]==target){
+        //         return mid;
+        //     }
+        //     else if(nums[mid]<target){
+        //         low = mid + 1;
+        //     }
+        //     else{
+        //         high = mid - 1;
+        //     }
+        // }
+        // return -1;
+        return bs(nums,0,nums.length-1,target);
+        
     }
 }
